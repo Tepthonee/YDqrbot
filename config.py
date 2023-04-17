14a4b58d1ea9e17b60fb860656c63d384e6e6ea7
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-04-17 20:05:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -21,11 +21,11 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "1260465030").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Tepthonee/QuranBot")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://t.me/T8OTT")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Tepthon")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/T8OTT")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/PPF22")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
