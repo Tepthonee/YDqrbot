6a6b2bda3acce290c06528ed1cf943ed70723e55
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-12-02 10:41:43 +0300

    update inline.py

--- AnonX/plugins/inline.py
+++ AnonX/plugins/inline.py
@@ -38,22 +38,22 @@ async def inline_query_handler(client, query):
                 [
                     [
                         InlineKeyboardButton(
-                            text="• ʏᴏᴜᴛᴜʙᴇ •",
+                            text="• يوتيـوب •",
                             url=link,
                         )
                     ],
                 ]
             )
             searched_text = f"""
-📌 **ᴛɪᴛʟᴇ:** [{title}]({link})
+📌 **العنوان :** [{title}]({link})
 
-⏳ **ᴅᴜʀᴀᴛɪᴏɴ:** {duration} Mins
-👀 **ᴠɪᴇᴡs:** `{views}`
-⏰ **ᴩᴜʙʟɪsʜᴇᴅ ᴏɴ:** {published}
-🎥 **ᴄʜᴀɴɴᴇʟ:** {channel}
-📎 **ᴄʜᴀɴɴᴇʟ ʟɪɴᴋ:** [ᴠɪsɪᴛ ᴄʜᴀɴɴᴇʟ]({channellink})
+⏳ **المدة :** {duration} Mins
+👀 **المشاهدات :** `{views}`
+⏰ **نشرت على :** {published}
+🎥 **القناة :** {channel}
+📎 **رابط القناة :** [زيـارة القنـاة]({channellink})
 
-💖 **sᴇᴀʀᴄʜ ᴩᴏᴡᴇʀᴇᴅ ʙʏ {MUSIC_BOT_NAME}**"""
+🔍 **تم البحث بواسطـة {MUSIC_BOT_NAME}**"""
             answers.append(
                 InlineQueryResultPhoto(
                     photo_url=thumbnail,
