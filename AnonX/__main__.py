c27168782b195e05dceb73a586aa7a28d1848554
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-12-02 11:10:01 +0300

    Update __main__.py

--- AnonX/__main__.py
+++ AnonX/__main__.py
@@ -69,4 +69,4 @@ async def init():
 
 if __name__ == "__main__":
     loop.run_until_complete(init())
-    LOGGER("AnonX").info("Stopping Music Bot...")
+    LOGGER("AnonX").info("تم إيقـاف البـوت...")
